09961aa182efe4a977bc06f0f0fd6303d474ce06
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-06-27 15:27:18 -0600

    added linter, listener, get route, and  error handling

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 4000 
+
+const app = express()
+
+// Middleware
+
+app.use(cors())
+app.use(morgan('dev'))
+
+// Router files
+
+// Routes
+app.get('/api/test', (req,res,next) => {
+    res.json({
+        message: 'Route working'
+    })
+})
+
+// Error handling
+
+    // The following 2 `app.use`'s MUST follow ALL your routes/middleware
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
+
+// Listener
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
