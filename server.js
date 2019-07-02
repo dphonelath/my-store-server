954ced8db739cbdc003d98e61b2176d9411b01cd
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-02 16:26:55 -0600

    added get route for products

--- server.js
+++ server.js
@@ -35,6 +35,20 @@ app.get('/api/categories', (req, res, next) => {
         });
 });
 
+app.get('/api/products', (req, res, next) =>{
+    Product.findAll({
+        include: [{ model:Category }]
+    })
+        .then(products => {
+            res.json({
+                products
+            });
+        })
+        .catch(error => {
+            next(error);
+        });
+});
+
 // Error handling
 
 // The following 2 `app.use`'s MUST follow ALL your routes/middleware
